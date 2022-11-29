0f29845b4fdbceac7864dead908dc6450a983ec1
Sven Hoyer <sven.hoyer@hotmail.com>
2022-11-29 11:59:00 +0100

    add logging config

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ from apps.sit_ble_mesh.mesh import Mesh
 
 def main():
     loop = asyncio.new_event_loop()
+    loop2 = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    mesh = Mesh(loop)
+    mesh = Mesh(loop2)
     socket = BleWebsocket(mesh)
 
     loop.run_until_complete(socket.connect())
